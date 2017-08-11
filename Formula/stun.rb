e1fda268c42c4b311e619c9b876540453f5ff5b2
Jiahao Li <isundaylee.reg@gmail.com>
2017-08-10 21:32:23 -0700

    Updates stun to v0.5.0

--- Formula/stun.rb
+++ Formula/stun.rb
@@ -1,9 +1,9 @@
 class Stun < Formula
-  version "0.4.0"
+  version "0.5.0"
   desc "A simple layer-3 network tunnel written in C++."
   homepage "https://github.com.com/isundaylee/stun"
-  url "https://github.com/isundaylee/stun/releases/download/v0.4.0/stun-osx.zip"
-  sha256 "16dcc84b701eb9eefb0a7e6316a2de7d4886e860a6b13bd0fef62ab52e9e64ab"
+  url "https://github.com/isundaylee/stun/releases/download/v0.5.0/stun-osx.zip"
+  sha256 "71cafe35aadd35548efcd88785931d2b0d70c94de59e6b6caa690c4ae0d76374"
 
   def install
     # Install the binary file
